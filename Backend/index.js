407128a7e7f49e3e3fc4f63051ed9e3dfff661d9
garbuzshay <shay.garbuz@e.braude.ac.il>
2024-07-30 22:31:40 +0300

    try to fix cors

--- Backend/index.js
+++ Backend/index.js
@@ -98,7 +98,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
