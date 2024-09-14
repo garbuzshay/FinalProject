f73a96386fd09b7c8dfd0148dd8a19c0fb28bbd9
garbuzshay <shay.garbuz@e.braude.ac.il>
2024-09-14 11:23:09 +0300

    fix visitor landing page

--- visitor-front/src/App.js
+++ visitor-front/src/App.js
@@ -1,3 +1,39 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import MuseumLoginPage from "./pages/MuseumLoginPage";
+// import MuseumPage from "./pages/MuseumPage";
+// import { MuseumProvider } from "./contexts/MuseumContext";
+// import ExhibitionPage from "./pages/ExhibitionPage";
+// import LandingPage from "./components/LandingPage";
+
+// const App = () => {
+//   return (
+//     <MuseumProvider>
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<MuseumLoginPage />} />
+
+//           {/* Landing Page for each museum */}
+//           <Route path="/:museumName" element={<LandingPage />} />
+
+//           {/* Museum Exhibitions list */}
+//           <Route path="/:museumName/exhibitions" element={<MuseumPage />} />
+
+//           {/* Single Exhibition Details */}
+//           <Route
+//             path="/:museumName/exhibitions/:exhibitionId"
+//             element={<ExhibitionPage />}
+//           />
+//           {/* Other routes... */}
+//         </Routes>
+//       </Router>
+//     </MuseumProvider>
+//   );
+// };
+
+// export default App;
+
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MuseumLoginPage from "./pages/MuseumLoginPage";
@@ -11,6 +47,7 @@ const App = () => {
     <MuseumProvider>
       <Router>
         <Routes>
+          {/* Museum login page for admin/museum owner */}
           <Route path="/" element={<MuseumLoginPage />} />
 
           {/* Landing Page for each museum */}
@@ -24,6 +61,7 @@ const App = () => {
             path="/:museumName/exhibitions/:exhibitionId"
             element={<ExhibitionPage />}
           />
+
           {/* Other routes... */}
         </Routes>
       </Router>
