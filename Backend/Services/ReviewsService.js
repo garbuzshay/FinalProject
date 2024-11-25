ad6bcb077ea132500aac797ed2e6f05404e80271
garbuzshay <shay.garbuz@e.braude.ac.il>
2024-11-25 17:31:10 +0200

    fix add review

--- Backend/Services/ReviewsService.js
+++ Backend/Services/ReviewsService.js
@@ -7,6 +7,44 @@ class ReviewsService {
    * Adds or updates feedback for a museum and its exhibitions.
    * @param {Object} feedbackData - Contains museumId, overallRating, and ratings for exhibitions.
    */
+  // async addOrUpdateFeedback(feedbackData) {
+  //   const { museumId, overallRating, exhibitionRatings } = feedbackData;
+
+  //   try {
+  //     let review = await ReviewModel.findOne({ museum: museumId });
+
+  //     if (!review) {
+  //       // Create new review record if none exists
+  //       const exhibitions = exhibitionRatings.map(({ exhibitionId, rating }) => ({
+  //         exhibitionId,
+  //         exhibitionScores: [rating],
+  //       }));
+
+  //       review = new ReviewModel({
+  //         museum: museumId,
+  //         museumScores: [overallRating],
+  //         exhibitions,
+  //       });
+  //     } else {
+  //       // Update existing review record
+  //       review.museumScores.push(overallRating);
+  //       exhibitionRatings.forEach(({ exhibitionId, rating }) => {
+  //         const exhibition = review.exhibitions.find(ex => ex.exhibitionId.toString() === exhibitionId);
+  //         if (exhibition) {
+  //           exhibition.exhibitionScores.push(rating);
+  //         } else {
+  //           review.exhibitions.push({ exhibitionScores: [rating] });
+  //         }
+  //       });
+  //     }
+
+  //     await review.save();
+  //     return review;
+  //   } catch (error) {
+  //     console.error('Error adding or updating feedback:', error);
+  //     throw error;
+  //   }
+  // }
   async addOrUpdateFeedback(feedbackData) {
     const { museumId, overallRating, exhibitionRatings } = feedbackData;
 
@@ -15,10 +53,12 @@ class ReviewsService {
 
       if (!review) {
         // Create new review record if none exists
-        const exhibitions = exhibitionRatings.map(({ exhibitionId, rating }) => ({
-          exhibitionId,
-          exhibitionScores: [rating],
-        }));
+        const exhibitions = exhibitionRatings
+          .filter(rating => rating.exhibitionId && rating.rating)
+          .map(({ exhibitionId, rating }) => ({
+            exhibitionId,
+            exhibitionScores: [rating],
+          }));
 
         review = new ReviewModel({
           museum: museumId,
@@ -29,14 +69,22 @@ class ReviewsService {
         // Update existing review record
         review.museumScores.push(overallRating);
 
-        exhibitionRatings.forEach(({ exhibitionId, rating }) => {
-          const exhibition = review.exhibitions.find(ex => ex.exhibitionId.toString() === exhibitionId);
-          if (exhibition) {
-            exhibition.exhibitionScores.push(rating);
-          } else {
-            review.exhibitions.push({ exhibitionScores: [rating] });
-          }
-        });
+        exhibitionRatings
+          .filter(rating => rating.exhibitionId && rating.rating)
+          .forEach(({ exhibitionId, rating }) => {
+            const exhibition = review.exhibitions.find(ex => 
+              ex.exhibitionId && ex.exhibitionId.toString() === exhibitionId.toString()
+            );
+
+            if (exhibition) {
+              exhibition.exhibitionScores.push(rating);
+            } else {
+              review.exhibitions.push({ 
+                exhibitionId, 
+                exhibitionScores: [rating] 
+              });
+            }
+          });
       }
 
       await review.save();
--- visitor-front/src/components/LogoutButton.js
+++ visitor-front/src/components/LogoutButton.js
@@ -238,7 +238,7 @@ const LogoutButton = () => {
                     onClick={handleFeedbackNo}
                     className="px-4 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 w-full md:w-auto"
                   >
-                    No
+                    No, leave without giving a review.
                   </button>
                 </div>
               </>
